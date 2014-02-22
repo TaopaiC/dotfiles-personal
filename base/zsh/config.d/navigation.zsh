847ea0da0e37e67e4145e9639a23c21bff4e610b
TaopaiC <pctao.blog@pctao.org>
2014-02-22 22:03:25 +0800

    [zsh] add navagation.zsh

---
+++ base/zsh/config.d/navigation.zsh
@@ -0,0 +1,12 @@
+export CURRENT_PROJECT_PATH=$HOME/.current-project
+
+function chpwd {
+  echo $(pwd) >! $CURRENT_PROJECT_PATH
+  ls
+}
+
+cd_to_most_recently_opened_directory() {
+  if [[ -f $CURRENT_PROJECT_PATH && -d "$(cat $CURRENT_PROJECT_PATH)" ]]; then
+    cd "$(cat $CURRENT_PROJECT_PATH)"
+  fi
+}
