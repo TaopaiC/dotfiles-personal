ff9c875e293f322b062240afa12a39b86b0a7983
TaopaiC <pctao.blog@pctao.org>
2014-02-26 23:20:17 +0800

    [ruby|zsh] temply fix rbenv PATH issue

--- common/ruby/zsh/config.d/rbenv.zsh
+++ common/ruby/zsh/config.d/rbenv.zsh
@@ -1,5 +1,5 @@
 # rbenv
-if [[ -z $TMUX  ]]; then
-  export PATH="$HOME/.rbenv/bin:$PATH"
+if [[ -z $TMUXX  ]]; then
+  export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
   if which rbenv > /dev/null; then eval "$(rbenv init - --no-rehash)"; fi
 fi
