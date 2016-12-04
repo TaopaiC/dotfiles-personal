15d11bf76b2b21d2a7a3f4d652c76a200e6d4d8c
TaopaiC <pctao.blog@pctao.org>
2016-12-04 21:31:57 +0800

    [zsh] lazy loading nvm script

--- common/node/zsh/config.d/nvm.zsh
+++ common/node/zsh/config.d/nvm.zsh
@@ -1,2 +1,7 @@
-[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh # This loads NVM
-plugins=($plugins npm nvm)
+lazy_source () {
+    eval "$1 () { [ -f $2 ] && source $2 && $1 \$@ }"
+}
+
+NVM_SOURCE=$HOME/.nvm/nvm.sh
+# This loads NVM
+lazy_source nvm $NVM_SOURCE
